4edc5d33e48e644b221fa52b5c4eb46f24c109cf
Jposeph99 <joseph.ahmed65@yahoo.com>
2023-06-21 00:30:25 -0600

    edit done

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 
 from app import app
 
-from flask import Flask,render_template,redirect, request , Blueprint
+from flask import Flask,render_template,redirect, request , Blueprint, flash
 import requests
 from .forms import pokemon_info
 from .forms import pokemon_info
@@ -16,21 +16,40 @@ def home():
 @app.route('/pokemon', methods=['GET','POST'])
 def pokemon():
     form = pokemon_info()
-    poke_name = name
-    url = f'https://pokeapi.co/api/v2/pokemon/{poke_name}'
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-        char={}
-        char['pok_name']=data['forms'][0]['name']
-        char['pok_hp']=data['stats'][0]['base_stat']
-        char['pok_attack']=data['stats'][1]['base_stat']
-        char['pok_defense']=data['stats'][2]['base_stat']
-        char['pok_ability']=(data['abilities'][0]['ability']['name'])
-        char['pok_experience']=data['base_experience']
-        char['pok_spirit_img']=data['sprites']['front_shiny']
-        return(char)
+    if request.method == 'POST':
+        poke_name = form.pokemon_name.data
+        url = f'https://pokeapi.co/api/v2/pokemon/{poke_name}'
+        response = requests.get(url)
+        if response.ok:
+            data = response.json()
+            char={}
+            char['pok_name']=data['forms'][0]['name']
+            char['pok_hp']=data['stats'][0]['base_stat']
+            char['pok_attack']=data['stats'][1]['base_stat']
+            char['pok_defense']=data['stats'][2]['base_stat']
+            char['pok_ability']=(data['abilities'][0]['ability']['name'])
+            char['pok_experience']=data['base_experience']
+            char['pok_spirit_img']=data['sprites']['front_shiny']
+            
+            return render_template('pokemon.html', form=form, char=char)
+        else:
+            print('Please make sure you enetered the right Pokemon')
+    # poke_name = name
+    # url = f'https://pokeapi.co/api/v2/pokemon/{poke_name}'
+    # response = requests.get(url)
+    # if response.ok:
+    #     data = response.json()
+    #     char={}
+    #     char['pok_name']=data['forms'][0]['name']
+    #     char['pok_hp']=data['stats'][0]['base_stat']
+    #     char['pok_attack']=data['stats'][1]['base_stat']
+    #     char['pok_defense']=data['stats'][2]['base_stat']
+    #     char['pok_ability']=(data['abilities'][0]['ability']['name'])
+    #     char['pok_experience']=data['base_experience']
+    #     char['pok_spirit_img']=data['sprites']['front_shiny']
+    #     return(char)
 #     else:
 #         return 'Please make sure you enetered the right Pokemon'
+
 
     return render_template('pokemon.html',form=form)
